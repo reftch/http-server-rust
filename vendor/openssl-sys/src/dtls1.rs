b3e46bad775887e551e6dbb2b487cd7b4dfe6b8d
reftch <j.developer@hotmail.com>
2026-07-20 23:41:13 +0200

    Removed vendor code

--- vendor/openssl-sys/src/dtls1.rs
+++
@@ -1,9 +0,0 @@
-use std::ffi::c_uint;
-
-cfg_if! {
-    if #[cfg(ossl300)] {
-        pub const DTLS1_COOKIE_LENGTH: c_uint = 255;
-    } else {
-        pub const DTLS1_COOKIE_LENGTH: c_uint = 256;
-    }
-}
--- vendor/openssl-sys/src/types.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-pub enum EVP_PKEY {}
-
-cfg_if! {
-    if #[cfg(ossl300)] {
-        pub enum EVP_SIGNATURE {}
-    }
-}
